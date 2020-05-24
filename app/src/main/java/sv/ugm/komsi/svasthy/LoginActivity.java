b68583c61b26e313e9ce16f1fc14ad7d6b893fcd
Yuli Hardina <yulihardinap@gmail.com>
2020-05-24 15:52:40 +0800

    auth

--- app/src/main/java/sv/ugm/komsi/svasthy/LoginActivity.java
+++ app/src/main/java/sv/ugm/komsi/svasthy/LoginActivity.java
@@ -51,5 +51,26 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(new Intent(this,RegisterActivity.class));
         overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
 
-    }
+
+   }
+   mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(TAG, "signInWithEmail:success");
+                FirebaseUser user = mAuth.getCurrentUser();
+                updateUI(user);
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+                updateUI(null);
+            }
+
+            // ...
+        }
+    });
 }
